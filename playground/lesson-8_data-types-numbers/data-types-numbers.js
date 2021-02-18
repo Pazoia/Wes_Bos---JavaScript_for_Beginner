c4497d48676f55ed2f057197a42797ea0a59fce7
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-18 13:09:47 +0000

    Completed lesson 8: data types numbers

---
+++ playground/lesson-8_data-types-numbers/data-types-numbers.js
@@ -0,0 +1,28 @@
+console.log(`data types numbers`);
+
+/* to find out the type of data you are working with:
+   on the console type typeof followed by the variable name or any other value */
+
+// example:
+const age = 100;
+// typeof age would return "number"
+const name = 'Paulo';
+// typeof name would return "string"
+
+// typeof 100 would also return "number"
+// typeof 'Paulo' would also return "string"
+
+// a number can be an integer which means a number without decimals
+const age1 = 200;
+
+// a number can also be a float which means a number with decimals
+const measure = 2.5;
+
+/* 
+with number you can use:
+multiplication: *
+substraction:   -
+division:       /
+addition:       +
+modulo:         % 
+*/
