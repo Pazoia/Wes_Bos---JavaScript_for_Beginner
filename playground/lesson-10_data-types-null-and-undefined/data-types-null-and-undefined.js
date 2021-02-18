1c71ee8d3d714dc76a8aee2cba6d548c497f45d3
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-18 16:20:02 +0000

    Completed lesson 10: data types null and undefined

---
+++ playground/lesson-10_data-types-null-and-undefined/data-types-null-and-undefined.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+console.log(`data types null and undefined`);
+
+// examples of undefined
+let dog;
+console.log(dog);
+dog = 'snickers';
+console.log(dog);
+
+// example of null
+const teller = {
+    first: 'Raymond',
+    last: 'Teller'
+}
+console.log(teller);
+
+teller.first = 'Teller';
+teller.last = null;
+console.log(teller);
