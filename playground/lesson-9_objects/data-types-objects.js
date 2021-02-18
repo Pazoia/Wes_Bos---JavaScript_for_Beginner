bacb90e822b234a8d2517ff14cfa0180c53f32df
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-18 16:02:58 +0000

    Completed lesson 9: data types objects

---
+++ playground/lesson-9_objects/data-types-objects.js
@@ -0,0 +1,11 @@
+console.log(`data types objects`);
+
+const person = {
+  first: 'Paulo',
+  last: 'Azoia',
+  age: 100,
+};
+
+console.log(
+  `Hello my name is ${person.first} ${person.last}, and I'm ${person.age} years old!!!`
+);
