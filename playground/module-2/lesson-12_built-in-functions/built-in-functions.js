8dbe1da881bba1ba6b7de5905ffcab483d730fdc
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-25 14:45:03 +0000

    Completed lesson 12 - built in functions

--- playground/module-2/lesson-12_built-in-functions/built-in-functions.js
+++ playground/module-2/lesson-12_built-in-functions/built-in-functions.js
@@ -1,1 +1,18 @@
 console.log('Built in Funtions');
+
+const max = Math.max(10, 15);
+console.log(`Math.max returns the higher number: ${max}`);
+
+const wholeNum = Math.floor(2.4444);
+console.log(`Math.floor on 2.4444 returns: ${wholeNum}`);
+
+const p = document.querySelector('p');
+const scrollToTop = document.querySelector('button');
+
+scrollToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
