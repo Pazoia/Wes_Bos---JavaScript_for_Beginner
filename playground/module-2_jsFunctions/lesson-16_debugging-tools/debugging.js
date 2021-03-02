32ec9492602b8d81272cb2fdab3a838855c41289
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-02 18:43:44 +0000

    Lesson 16 - Debugging Tools Complete

--- playground/module-2_jsFunctions/lesson-16_debugging-tools/debugging.js
+++ playground/module-2_jsFunctions/lesson-16_debugging-tools/debugging.js
@@ -1,10 +1,10 @@
 console.log("Debugging Tools");
 
-const people = [
-    { name: "Wes", cool: true, country: "Canada" },
-    { name: "Scott", cool: true, country: "Merica" },
-    { name: "Snickers", cool: false, country: "Dog Country" },
-];
+// const people = [
+//     { name: "Wes", cool: true, country: "Canada" },
+//     { name: "Scott", cool: true, country: "Merica" },
+//     { name: "Snickers", cool: false, country: "Dog Country" },
+// ];
 
 // Console Methods
 /*
@@ -64,25 +64,26 @@ const people = [
 // Scope
 
 // Network Requests
+/*
+    // A Dad joke fetch
+    async function fetchDadJoke() {
+        const res = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "text/plain",
+            },
+        });
+        const joke = await res.text();
+        console.log(joke);
+        return joke;
+    }
+*/
 
 // Break On Attribute
-
-// Some Setup Code
-
-const button = document.querySelector(".bigger");
-button.addEventListener("click", function (e) {
-    const newFontSize = parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
-    e.currentTarget.style.fontSize = `${newFontSize}px`;
-});
-
-// A Dad joke fetch
-async function fetchDadJoke() {
-    const res = await fetch("https://icanhazdadjoke.com/", {
-        headers: {
-            Accept: "text/plain",
-        },
+/*
+    const button = document.querySelector(".bigger");
+    button.addEventListener("click", function (e) {
+        const newFontSize = parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+        e.currentTarget.style.fontSize = `${newFontSize}px`;
     });
-    const joke = await res.text();
-    console.log(joke);
-    return joke;
-}
+*/
+// Some Setup Code
