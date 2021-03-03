220838c14991b6fac02f9cb1b1cc3ac82c7fe961
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-03 21:23:07 +0000

    Lesson 18 - Hoisting Complete

--- playground/module3_jsTrickyBits/lesson-18_hoisting/hoisting.js
+++ playground/module3_jsTrickyBits/lesson-18_hoisting/hoisting.js
@@ -1,1 +1,18 @@
 console.log("Hoisting");
+
+// Function hoisting
+/*
+    function hoisting means that JavaScript will move all functions
+    declared with the function keyword to the top of the file.
+    This means you can call the function before it is declared and JavaScript
+    will still running with no problems.
+    example:
+*/
+
+// Calling the function
+sayHi();
+
+// Declaring the function
+function sayHi() {
+    console.log("Hey!!!");
+}
