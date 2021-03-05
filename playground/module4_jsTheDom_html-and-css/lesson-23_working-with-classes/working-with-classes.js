2ccfdc030ed68c36cd1e29fe52f4c93eb1230fd1
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-05 14:38:21 +0000

    Lesson 23 - Working with Classes Complete

--- playground/module4_jsTheDom_html-and-css/lesson-23_working-with-classes/working-with-classes.js
+++ playground/module4_jsTheDom_html-and-css/lesson-23_working-with-classes/working-with-classes.js
@@ -1,1 +1,20 @@
 console.log("Working with Classes");
+
+const pic = document.querySelector(".nice");
+console.log(pic.classList);
+
+// Adding a class to an element tag
+pic.classList.add("open");
+console.log(pic.classList);
+
+// Removing a class from an element tag
+pic.classList.remove("cool");
+console.log(pic.classList);
+
+// Adding and removing a class to an element using the toggle method
+function toggleRound() {
+    pic.classList.toggle("round");
+    console.log(pic.classList);
+}
+
+pic.addEventListener("click", toggleRound);
