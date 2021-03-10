f43b2bf9925af374b4b01dfd20760e6889193c41
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-10 17:59:21 +0000

    Lesson 27 - Traversing and Removing nodes Complete

--- playground/module4_jsTheDom_html-and-css/lesson-27_traversing-and-removing-nodes/traversing-and-removing-nodes.js
+++ playground/module4_jsTheDom_html-and-css/lesson-27_traversing-and-removing-nodes/traversing-and-removing-nodes.js
@@ -1,1 +1,42 @@
 console.log("Traversing and Removing nodes");
+
+const image = document.querySelector(".cool");
+image.classList.add("round");
+
+/*
+    <p class="paulo">
+        "I am Paulo, I"  // I am a node
+        <em>love</em>  // I am a node
+        " to bbq and "  // I am a node
+        <strong>Learning javascript</strong>  // I am a node
+        "!!!"  // I am a node
+    </p>
+*/
+
+const paulo = document.querySelector(".paulo");
+
+// Below are some properties that apply to Elements
+console.log(paulo.children);
+console.log(paulo.firstElementChild);
+console.log(paulo.lastElementChild);
+console.log(paulo.previousElementSibling);
+console.log(paulo.nextElementSibling);
+console.log(paulo.parentElement);
+
+// Below are some properties that apply to Nodes
+console.log(paulo.childNodes);
+console.log(paulo.firstChild);
+console.log(paulo.lastChild);
+console.log(paulo.lastChild.previousSibling);
+console.log(paulo.firstChild.nextSibling);
+console.log(paulo.parentNode);
+
+const p = document.createElement("p");
+p.textContent = "I will be removed";
+document.body.appendChild(p);
+
+p.remove();
+
+console.log(p);
+
+document.body.appendChild(p);
