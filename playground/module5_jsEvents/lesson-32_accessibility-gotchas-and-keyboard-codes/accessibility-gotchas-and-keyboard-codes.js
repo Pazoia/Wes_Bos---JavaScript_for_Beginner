03efa3cb8eb666b6edbdcdd7aaed5f6c8658c5f8
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-17 16:49:17 +0000

    Accessibility gotchas and Keyboard codes Complete

--- playground/module5_jsEvents/lesson-32_accessibility-gotchas-and-keyboard-codes/accessibility-gotchas-and-keyboard-codes.js
+++ playground/module5_jsEvents/lesson-32_accessibility-gotchas-and-keyboard-codes/accessibility-gotchas-and-keyboard-codes.js
@@ -1,1 +1,17 @@
 console.log("accessibility gotchas and keyboard codes");
+
+const photo = document.querySelector(".photo");
+
+function handlePhotoClick(event) {
+    // This is to avoid the console.log() command to run unless
+    // there's a click event or pressing the Enter key.
+    if (event.type === "click" || event.key === "Enter") {
+        console.log("You clicked the photo!!!");
+    }
+}
+
+// This will trigger the handlePhotoClick function with the click of the mouse
+photo.addEventListener("click", handlePhotoClick);
+
+// This will trigger the handlePhotoClick function by pressing the Enter key
+photo.addEventListener("keyup", handlePhotoClick);
