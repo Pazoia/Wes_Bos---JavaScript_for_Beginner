ff9ae6720286805bcc3cf94b481c2b0b218019d9
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-18 16:15:44 +0000

    objectives written in pseudocode

--- playground/module6_jsSeriousPractiseExercises/lesson-33_etch-a-sketch/etch-a-sketch.js
+++ playground/module6_jsSeriousPractiseExercises/lesson-33_etch-a-sketch/etch-a-sketch.js
@@ -1,1 +1,13 @@
 console.log("Etch a Sketch");
+
+// Select the elements on the page - canvas, shake button
+
+// Setup our canvas for drawing
+
+// Write a draw function
+
+// Write a handler for the keys
+
+// Clear / shake function
+
+// Listen for the arrow keys
