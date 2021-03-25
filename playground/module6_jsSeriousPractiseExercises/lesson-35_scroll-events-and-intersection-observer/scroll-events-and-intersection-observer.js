b9881813874ec97db1245dc1eabdf07a5fd2fb90
Paulo Azoia <pazoia.gomes@gmail.com>
2021-03-25 20:39:43 +0000

    Lesson 35 - Scroll events and Intersection observer Complete

--- playground/module6_jsSeriousPractiseExercises/lesson-35_scroll-events-and-intersection-observer/scroll-events-and-intersection-observer.js
+++ playground/module6_jsSeriousPractiseExercises/lesson-35_scroll-events-and-intersection-observer/scroll-events-and-intersection-observer.js
@@ -1,1 +1,21 @@
 console.log("Scroll events and Intersection observer");
+
+const terms = document.querySelector(".terms-and-conditions");
+// const watch = document.querySelector(".watch");
+const button = document.querySelector(".accept");
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+});
+
+function obCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+        console.log("Wroking");
+        // Stop observing the button once its clickable
+        ob.unobserve(terms.lastElementChild);
+    }
+}
+
+ob.observe(terms.lastElementChild);
