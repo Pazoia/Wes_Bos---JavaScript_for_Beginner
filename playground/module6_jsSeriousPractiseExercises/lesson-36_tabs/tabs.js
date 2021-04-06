e817550ae676a1092ae8d5f42aa7b8b33fee0602
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-06 16:13:15 +0100

    Lesson 36 - Working with tabs Complete

--- playground/module6_jsSeriousPractiseExercises/lesson-36_tabs/tabs.js
+++ playground/module6_jsSeriousPractiseExercises/lesson-36_tabs/tabs.js
@@ -2,7 +2,7 @@ console.log("Working with tabs!!!");
 
 const tabs = document.querySelector(".tabs");
 const tabButtons = tabs.querySelectorAll("[role='tab']");
-const tabPanels = tabs.querySelectorAll("[role='tabpanel']");
+const tabPanels = Array.from(tabs.querySelectorAll("[role='tabpanel']"));
 
 function handleTabClick(event) {
     // hide all tab panels
@@ -14,7 +14,19 @@ function handleTabClick(event) {
         tab.setAttribute("aria-selected", false);
     });
     // mark the clicked tab as selected
+    event.currentTarget.setAttribute("aria-selected", true);
     // find the associated tabPanel and show it
+    const { id } = event.currentTarget;
+
+    /*
+        METHOD 1:
+    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    tabPanel.hidden = false;
+    */
+
+    // METHOD 2 - find in the array of tabPanels
+    const tabPanel = tabPanels.find((panel) => panel.getAttribute("aria-labelledby") === id);
+    tabPanel.hidden = false;
 }
 
 tabButtons.forEach((button) => button.addEventListener("click", handleTabClick));
