18d46a6361bc6b31628957d518ab0777de432498
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-06 22:13:10 +0100

    Lesson 39 - Coercion, Ternaries and Conditional abuse Complete

--- playground/module7_jsLogicAndFlowControl/lesson-39_coercion-ternaries-and-conditional-abuse/coercion-ternaries-and-conditional-abuse.js
+++ playground/module7_jsLogicAndFlowControl/lesson-39_coercion-ternaries-and-conditional-abuse/coercion-ternaries-and-conditional-abuse.js
@@ -1,1 +1,23 @@
 console.log("Coercion, Ternaries and Conditional abuse");
+
+// Coercion: is when one value is coerced into a diferent type
+const isCool = true;
+
+if (!isCool) {
+    console.log("You are not cool");
+}
+
+// Ternary
+const count = 10;
+/*
+let word;
+if (count === 1) {
+    word = "item";
+} else {
+    word = "items";
+}
+*/
+
+const word = count === 1 ? "item" : "items";
+const sentence = `You have ${count} ${word} in your cart`;
+console.log(sentence);
