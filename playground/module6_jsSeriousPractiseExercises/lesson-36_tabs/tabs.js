fbd6b3bd7c64b17adb5af171d00e44e36d78ce3f
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-06 15:49:09 +0100

    function handleTabClick: hidding all the panels unselecting all the tabs

--- playground/module6_jsSeriousPractiseExercises/lesson-36_tabs/tabs.js
+++ playground/module6_jsSeriousPractiseExercises/lesson-36_tabs/tabs.js
@@ -1,1 +1,20 @@
 console.log("Working with tabs!!!");
+
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll("[role='tab']");
+const tabPanels = tabs.querySelectorAll("[role='tabpanel']");
+
+function handleTabClick(event) {
+    // hide all tab panels
+    tabPanels.forEach((panel) => {
+        panel.hidden = true;
+    });
+    // mark all tabs as unselected
+    tabButtons.forEach((tab) => {
+        tab.setAttribute("aria-selected", false);
+    });
+    // mark the clicked tab as selected
+    // find the associated tabPanel and show it
+}
+
+tabButtons.forEach((button) => button.addEventListener("click", handleTabClick));
