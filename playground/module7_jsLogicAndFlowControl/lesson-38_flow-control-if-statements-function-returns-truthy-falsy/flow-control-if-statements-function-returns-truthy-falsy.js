644a5c17b8deaf4bd4de08214e7d603ff3967658
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-06 21:39:28 +0100

    Lesson 38 - Flow Control Complete: If statements function return truthy or falsy

--- playground/module7_jsLogicAndFlowControl/lesson-38_flow-control-if-statements-function-returns-truthy-falsy/flow-control-if-statements-function-returns-truthy-falsy.js
+++ playground/module7_jsLogicAndFlowControl/lesson-38_flow-control-if-statements-function-returns-truthy-falsy/flow-control-if-statements-function-returns-truthy-falsy.js
@@ -1,1 +1,38 @@
 console.log("Flow control - if statements, function returns, truthy, falsy");
+
+const age = 100;
+if (age > 70 && age < 80) {
+    console.log("You're in your 70's");
+} else if (age > 60 && age < 70) {
+    console.log("You're in you 60's");
+} else if (age > 50 && age < 60) {
+    console.log("You're in you 50's");
+} else {
+    console.log("Nothing was true!");
+}
+
+function slugify(sentence, lowercase) {
+    if (lowercase) {
+        return sentence.replace(/\s/g, "-").toLowerCase();
+    }
+    return sentence.replace(/\s/g, "-");
+}
+
+console.log(slugify("You are cool"));
+console.log(slugify("I WAS ALL UPPERCASE!!!", true));
+
+const dog = "";
+
+if (dog) {
+    console.log("You have a dog");
+} else {
+    console.log("You don't have a dog");
+}
+
+const score = {};
+
+if (score) {
+    console.log("There's a score already");
+} else {
+    console.log("There's no score yet");
+}
