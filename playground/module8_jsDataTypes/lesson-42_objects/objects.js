f033d93f18264c3a2765b41c068835038f8eb0a8
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-07 16:18:29 +0100

    Lesson 42 - Objects Complete

--- playground/module8_jsDataTypes/lesson-42_objects/objects.js
+++ playground/module8_jsDataTypes/lesson-42_objects/objects.js
@@ -1,1 +1,32 @@
 console.log("Objects");
+
+const age = 100;
+const person = {
+    first: "Paulo",
+    last: "Azoia",
+    age,
+    "really cool": true,
+    "cool-dude": false,
+    clothing: {
+        shirts: 10,
+        pants: 2,
+    },
+    sayHello(greeting = "Hey") {
+        return `${greeting} ${this.first}`;
+    },
+};
+
+person.job = "developer to be!!!";
+person.age = 55;
+
+console.log(person);
+console.log(person.clothing.pants);
+console.log(person["cool-dude"]);
+
+const personpants = person.clothing?.pants;
+console.log(`I have ${personpants} pants`);
+
+// const propertyToCheck = prompt("What do you want to check?");
+
+// console.log(propertyToCheck);
+// console.log(person[propertyToCheck]);
