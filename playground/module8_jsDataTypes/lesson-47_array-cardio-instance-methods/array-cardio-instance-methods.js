4cc358c7783c156860bd14a43798f1bddc175d4a
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-12 22:25:15 +0100

    Lesson 47 - Array Cardio - Instance Methods Complete

--- playground/module8_jsDataTypes/lesson-47_array-cardio-instance-methods/array-cardio-instance-methods.js
+++ playground/module8_jsDataTypes/lesson-47_array-cardio-instance-methods/array-cardio-instance-methods.js
@@ -1,5 +1,6 @@
 console.log("Working with Arrays - Instance Methods");
 
+/* eslint-disable */
 const toppings = [
     "Mushrooms ",
     "Tomatoes",
@@ -42,40 +43,105 @@ const feedback = [
 /*
     Instance Methods
 */
-
+console.group("Instance Methods");
 // Display all bun types with " or " - use join()
+console.groupCollapsed(".join()");
+console.log(buns.join(" or "));
+console.groupEnd(".join()");
 
-// We have a string "hot dogs,hamburgers,sausages,corn" - use split() to turn it into a string
+// We have a string "hot dogs,hamburgers,sausages,corn" - use split() to turn it into an array
+console.groupCollapsed(".split()");
+const foodString = "hot dogs,hamburgers,sausages,corn";
+console.log(foodString.split(","));
+console.groupEnd(".split()");
 
 // take the last item off toppings with pop()
+console.groupCollapsed(".pop()");
+const lastItem = toppings.pop();
+console.log(`lastItem: ${lastItem}`);
+console.groupEnd(".pop()");
+
 // add it back with push()
+console.groupCollapsed(".push()");
+toppings.push(lastItem);
+console.log(toppings);
+console.groupEnd(".push()");
+
 // take the first item off toppings with shift()
+console.groupCollapsed(".shift()");
+const firstItem = toppings.shift();
+console.log(`firstItem: ${firstItem}`);
+console.log(toppings);
+console.groupEnd(".shift()");
+
 // add it back in with unshift()
+console.groupCollapsed(".unshift()");
+toppings.unshift(firstItem);
+console.log(toppings);
+console.groupEnd(".unshift()");
+
 // Do the last four,but immutable (with spreads and new variables)
+console.groupCollapsed(".pop, .push, .shift, .unshift using the spread operator (...)");
+console.log(".pop() Immutable, not changing the original array");
+let newToppings = toppings.slice(0, toppings.length - 1);
+console.log(newToppings);
+
+console.log(".push() Immutable, not changing the original array");
+newToppings = [...newToppings, toppings[toppings.length - 1]];
+console.log(newToppings);
+
+console.log(".shift() Immutable, not changing the original array");
+let newToppings1 = toppings.slice(1);
+console.log(newToppings1);
+
+console.log(".unshift() Immutable, not changing the original array");
+newToppings1 = [toppings[0], ...newToppings1];
+console.log(newToppings1);
+console.groupEnd(".pop, .push, .shift, .unshift using the spread operator (...)");
 
 // Make a copy of the toppings array with slice()
+console.groupCollapsed(".slice()");
+const toppingsCopy1 = toppings.slice();
+console.log(toppingsCopy1);
+console.groupEnd(".slice()");
+
 // Make a copy of the toppings array with a spread
+console.groupCollapsed("Making a copy of an array using the spread operator (...)");
+const toppingsCopy2 = [...toppings];
+console.log(toppingsCopy2);
+console.groupEnd("Making a copy of an array using the spread operator (...)");
+
 // take out items 3 to 5 of your new toppings array with splice()
+console.groupCollapsed(".splice()");
+const toppingsRemoved = toppingsCopy1.splice(3, 3);
+console.log(toppingsCopy1);
+console.groupEnd(".splice()");
+
 // find the index of Avocado with indexOf() / lastIndexOf()
+console.groupCollapsed(".indexOf()");
+const avoIndex = toppings.indexOf("Avocado");
+console.log(avoIndex);
+console.groupEnd(".indexOf()");
+
 // Check if hot sauce is in the toppings with includes()
+console.groupCollapsed(".includes()");
+function isInToppings(topping) {
+    return toppings.includes(topping);
+}
+console.log("hot sauce in toppings:");
+console.log(isInToppings("hot sauce"));
+console.log("Bacon in toppings:");
+console.log(isInToppings("Bacon"));
 // add it if it's not
+if (!isInToppings("hot sauce")) {
+    toppings.push("hot sauce");
+};
+console.log(toppings);
+console.groupEnd(".includes()");
 // flip those toppings around with reverse()
+console.groupCollapsed(".reverse()");
+const reversedArray = [...toppings].reverse();
+console.log(reversedArray);
+console.groupEnd(".reverse()");
+console.groupEnd("Instance Methods");
 
-/*
-    Callback Methods
-*/
-
-// find the first rating that talks about a burger with find()
-// find all ratings that are above 2 with filter()
-// find all ratings that talk about a burger with filter()
-// Remove the one star rating however you like!
-
-// check if there is at least 5 of one type of meat with some()
-// make sure we have at least 3 of every meat with every()
-// sort the toppings alphabetically with sort()
-// sort the order totals from most expensive to least with .sort()
-// Sort the prices with sort()
-
-/*
-    Looping Methods (next)
-*/
