cf6798db7799cd46ef684dd2225b1b6648a7f94b
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-12 22:28:37 +0100

    Lesson 48 - Array Cardio - Callback Methods Starting

---
+++ playground/module8_jsDataTypes/lesson-48_array-cardio-callback-methods-and-function-generation/array-cardio-callback-methods-and-function-generation.js
@@ -0,0 +1,60 @@
+console.log("Working with Arrays - Callback Methods and Function Generation");
+
+/* eslint-disable */
+const toppings = [
+    "Mushrooms ",
+    "Tomatoes",
+    "Eggs",
+    "Chili",
+    "Lettuce",
+    "Avocado",
+    "Chiles",
+    "Bacon",
+    "Pickles",
+    "Onions",
+    "Cheese",
+];
+
+const buns = ["egg", "wonder", "brioche"];
+
+const meats = {
+    beyond: 10,
+    beef: 5,
+    pork: 7,
+};
+
+const prices = {
+    hotDog: 453,
+    burger: 765,
+    sausage: 634,
+    corn: 234,
+};
+
+const orderTotals = [342, 1002, 523, 34, 634, 854, 1644, 2222];
+
+const feedback = [
+    { comment: "Love the burgs", rating: 4 },
+    { comment: "Horrible Service", rating: 2 },
+    { comment: "Smoothies are great, liked the burger too", rating: 5 },
+    { comment: "Ambiance needs work", rating: 3 },
+    { comment: "I DONT LIKE BURGERS", rating: 1 },
+];
+
+/*
+    Callback Methods
+*/
+
+// find the first rating that talks about a burger with find()
+// find all ratings that are above 2 with filter()
+// find all ratings that talk about a burger with filter()
+// Remove the one star rating however you like!
+
+// check if there is at least 5 of one type of meat with some()
+// make sure we have at least 3 of every meat with every()
+// sort the toppings alphabetically with sort()
+// sort the order totals from most expensive to least with .sort()
+// Sort the prices with sort()
+
+/*
+    Looping Methods (next)
+*/
