a3e62a46e685053e4917760946bfaf6fa0c6452e
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-14 19:37:53 +0100

    Lesson 49 - Looping and Iterating - .forEach() Complete

--- playground/module9_jsGettinLoopy/lesson-49_forEach/forEach.js
+++ playground/module9_jsGettinLoopy/lesson-49_forEach/forEach.js
@@ -15,6 +15,78 @@ const toppings = [
     "Cheese",
 ];
 
+const students = [
+    {
+        id: "11ce",
+        first_name: "Dall",
+        last_name: "Puckring",
+    },
+    {
+        id: "2958",
+        first_name: "Margarete",
+        last_name: "Brandi",
+    },
+    {
+        id: "565a",
+        first_name: "Bendicty",
+        last_name: "Woodage",
+    },
+    {
+        id: "3a16",
+        first_name: "Micki",
+        last_name: "Mattes",
+    },
+    {
+        id: "f396",
+        first_name: "Flory",
+        last_name: "Gladeche",
+    },
+    {
+        id: "de5f",
+        first_name: "Jamill",
+        last_name: "Emilien",
+    },
+    {
+        id: "54cb",
+        first_name: "Brett",
+        last_name: "Aizikowitz",
+    },
+    {
+        id: "9135",
+        first_name: "Lorry",
+        last_name: "Smallman",
+    },
+    {
+        id: "978f",
+        first_name: "Gilly",
+        last_name: "Flott",
+    },
+];
+
+const people = [
+    {
+        birthday: "April 22, 1993",
+        names: {
+            first: "Keith",
+            last: "Buckley",
+        },
+    },
+    {
+        birthday: "January 3, 1975",
+        names: {
+            first: "Larry",
+            last: "Heep",
+        },
+    },
+    {
+        birthday: "February 12, 1944",
+        names: {
+            first: "Linda",
+            last: "Bermeer",
+        },
+    },
+];
+
 const buns = ["egg", "wonder", "brioche"];
 
 const meats = {
@@ -39,3 +111,39 @@ const feedback = [
     { comment: "Ambiance needs work", rating: 3 },
     { comment: "I DONT LIKE BURGERS", rating: 1 },
 ];
+
+const faces = ["😃", "🤠", "🤡", "🤑", "😵", "🌞", "🐶", "😺"];
+
+const inventory = [
+    { type: "shirt", price: 4000 },
+    { type: "pants", price: 4532 },
+    { type: "socks", price: 234 },
+    { type: "shirt", price: 2343 },
+    { type: "pants", price: 2343 },
+    { type: "socks", price: 542 },
+    { type: "pants", price: 123 },
+];
+
+function logTopping(topping, index, originalArray) {
+    const nextTopping = originalArray[index + 1];
+    const prevTopping = originalArray[index - 1];
+    // log the topping
+    console.log(`Topping: ${topping}`);
+
+    // log the prev topping if there is one
+    prevTopping ? console.log(`Prev topping: ${prevTopping}`) : null;
+
+    // log the next topping if there is one
+    nextTopping ? console.log(`Next topping: ${nextTopping}`) : null;
+
+    // if it is the last item in the array, say good bye
+    index === originalArray.length - 1 
+    ? console.log("Goodbye") 
+    : console.log("Getting another topping");
+
+    // Abusing the && conditional:
+    // index === originalArray.length - 1 && console.log("Goodbye");
+    console.log("----------🍕🍕🍕-----------");
+    
+}
+toppings.forEach(logTopping);
