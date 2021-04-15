72f86ac278f633a86a319b9fbda4caea339bf7ef
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-15 19:58:37 +0100

    Lesson 54 - Looping and Iterating Complete
    for, for of, for in, while

--- playground/module9_jsGettinLoopy/lesson-54_for-for_in-for_of-while-loops/for-forIn-forOf-while-loops.js
+++ playground/module9_jsGettinLoopy/lesson-54_for-for_in-for_of-while-loops/for-forIn-forOf-while-loops.js
@@ -1,1 +1,50 @@
 console.log("Working with for ; for of ; for in ; while loops");
+/* eslint-disable */
+
+const numbers = [2, 34, 3, 23, 42, 3, 1, 65, 364, 5, 645, 6];
+
+const myName = "Paulo Azoia";
+
+// For
+console.groupCollapsed("For loop");
+
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < 10; i++) {
+    console.log(myName);
+    console.log(`I printed ${i + 1} times`);
+}
+
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+console.groupEnd("For loop");
+// For of
+console.groupCollapsed("For of loop");
+for (const letter of myName) {
+    console.log(letter);
+}
+console.groupEnd("For of loop");
+
+// For in
+console.groupCollapsed("For in loop");
+
+for (const number in numbers) {
+    console.log(number);
+}
+console.groupEnd("For in loop");
+
+// While
+console.groupCollapsed("While loop");
+
+let cool = true;
+let i = 1;
+
+while (cool === true) {
+    console.log("You are cool");
+    i++;
+    if (i > 100) {
+        cool = false;
+    }
+}
+console.groupEnd("While loop");
