c5037b44e5ae66945aa101a2e2dc86afe6fe003e
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-19 13:13:59 +0100

    Text written in text box being displayed underneath

--- playground/module10_jsHarderPractiseExercises/lesson-56_sarcastic-text-generator/sarcastic-text-generator.js
+++ playground/module10_jsHarderPractiseExercises/lesson-56_sarcastic-text-generator/sarcastic-text-generator.js
@@ -1,1 +1,12 @@
 console.log("Sarcastic Text Generator");
+
+const textarea = document.querySelector("[name='text']");
+const result = document.querySelector(".result");
+const filterInputs = document.querySelectorAll("[name='filter']");
+
+function transformText(text) {
+    console.log(text);
+    result.textContent = text;
+}
+
+textarea.addEventListener("input", (e) => transformText(e.target.value));
