d0296f19b49b78ab20ab6f02ba66988c196b7939
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-19 20:00:17 +0100

    State being save to items array when submiting action

--- playground/module10_jsHarderPractiseExercises/lesson-57_shopping-form/shopping-list.js
+++ playground/module10_jsHarderPractiseExercises/lesson-57_shopping-form/shopping-list.js
@@ -1,1 +1,22 @@
 console.log("Shopping form - Custom Events - Delegation - LocalStorage");
+
+const shoppingForm = document.querySelector(".shopping");
+const list = document.querySelector(".list");
+
+// We need an array to hold our state
+const items = [];
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submited!!!");
+    const name = e.currentTarget.item.value;
+    const item = {
+        name,
+        id: Date.now(),
+        complete: false,
+    };
+    items.push(item);
+    e.target.reset();
+}
+
+shoppingForm.addEventListener("submit", handleSubmit);
