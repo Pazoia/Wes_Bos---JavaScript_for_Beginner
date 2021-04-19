f908ab3bdb59ac8107cd1686c283bc5c05735b0d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-19 13:54:11 +0100

    Sarcastic mode working, upperCase every other letter

--- playground/module10_jsHarderPractiseExercises/lesson-56_sarcastic-text-generator/sarcastic-text-generator.js
+++ playground/module10_jsHarderPractiseExercises/lesson-56_sarcastic-text-generator/sarcastic-text-generator.js
@@ -2,11 +2,29 @@ console.log("Sarcastic Text Generator");
 
 const textarea = document.querySelector("[name='text']");
 const result = document.querySelector(".result");
-const filterInputs = document.querySelectorAll("[name='filter']");
+const filterInputs = Array.from(document.querySelectorAll("[name='filter']"));
+
+const filters = {
+    sarcastic(letter, index) {
+        // if it is odd, it will return 1, and that is truthy
+        // so this if statement will trip
+        if (index % 2) {
+            return letter.toUpperCase();
+        }
+        // if it's even, it will return 0 and we will lowercase it.
+        return letter.toLowerCase();
+    },
+    funky() {},
+    unable() {},
+};
 
 function transformText(text) {
-    console.log(text);
-    result.textContent = text;
+    // const filter = document.querySelector("[name='filter']:checked").value;
+    const filter = filterInputs.find((input) => input.checked).value;
+
+    // take the text, and loop over each letter.
+    const mod = Array.from(text).map(filters[filter]);
+    result.textContent = mod.join("");
 }
 
 textarea.addEventListener("input", (e) => transformText(e.target.value));
