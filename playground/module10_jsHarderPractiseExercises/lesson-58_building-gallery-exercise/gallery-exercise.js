3c6b6a0cf34a2d3c57124b6443a4b860864280bc
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-22 14:08:10 +0100

    Modal next and previous buttons working:
    when clicked on
    also pictures being selected with the arrow keys.

--- playground/module10_jsHarderPractiseExercises/lesson-58_building-gallery-exercise/gallery-exercise.js
+++ playground/module10_jsHarderPractiseExercises/lesson-58_building-gallery-exercise/gallery-exercise.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-useless-return */
 function Gallery(gallery) {
     if (!gallery) {
@@ -11,6 +12,26 @@ function Gallery(gallery) {
     const nextButton = modal.querySelector(".next");
     let currentImage;
 
+    function handleClickOutside(e) {
+        if (e.target === e.currentTarget) {
+            closeModal();
+        }
+    }
+
+    function handleKeyUp(e) {
+        if (e.key === "Escape") return closeModal();
+        if (e.key === "ArrowRight") return showNextImage();
+        if (e.key === "ArrowLeft") return showPrevImage();
+    }
+
+    function showNextImage() {
+        showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+    }
+
+    function showPrevImage() {
+        showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+    }
+
     function openModal() {
         // First check if the modal is already open
         if (modal.matches(".open")) {
@@ -18,6 +39,21 @@ function Gallery(gallery) {
             return; // Stop the function from running
         }
         modal.classList.add("open");
+
+        // Event Listeners to be bound when we open the modal
+        window.addEventListener("keyup", handleKeyUp);
+        nextButton.addEventListener("click", showNextImage);
+        prevButton.addEventListener("click", showPrevImage);
+    }
+
+    function closeModal() {
+        modal.classList.remove("open");
+        // TODO: add event listeners for clicks and keyboards
+
+        // Event Listeners to be debound when we open the modal
+        window.removeEventListener("keyup", handleKeyUp);
+        nextButton.removeEventListener("click", showNextImage);
+        prevButton.removeEventListener("click", showPrevImage);
     }
 
     function showImage(el) {
@@ -33,11 +69,16 @@ function Gallery(gallery) {
         openModal();
     }
 
+    /*
+        These are our event listeners
+    */
     // prettier-ignore
     images.forEach((image) => 
         image.addEventListener("click", (e) =>
             showImage(e.currentTarget)
     ));
+
+    modal.addEventListener("click", handleClickOutside);
 }
 
 // Use it on the page
