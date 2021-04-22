ec6f4abdb759c1bf190e9eafe067b02fa096b9aa
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-22 19:59:58 +0100

    Lesson 61 - The 'this' keyword Complete

--- playground/module11_jsPrototypes-This-New-and-Inheritance/lesson-61_this-keyword/this-keyword.js
+++ playground/module11_jsPrototypes-This-New-and-Inheritance/lesson-61_this-keyword/this-keyword.js
@@ -1,1 +1,34 @@
 console.log("The 'this' keyword");
+
+const button1 = document.querySelector(".one");
+const button2 = document.querySelector(".two");
+
+function tellMeAboutTheButton() {
+    console.log("outside", this);
+    setTimeout(() => {
+        console.log("inside", this);
+        this.textContent = "You Clicked Me";
+    }, 1000);
+}
+
+button1.addEventListener("click", tellMeAboutTheButton);
+button2.addEventListener("click", tellMeAboutTheButton);
+
+function Pizza(toppings = [], customer) {
+    console.log("Making a pizza");
+    // save the toppings that were passed in,
+    // to this instance of pizza
+    this.toppings = toppings;
+    this.customer = customer;
+    this.id = Math.floor(Math.random() * 16777215).toString(16);
+}
+
+/*
+    Below are two instances of the pizza function,
+    create with the 'this' keyword to store the data
+    that we passed in to each of them.
+*/
+const salamiPizza = new Pizza(["salami", "cheese", "olives"], "Paulo Azoia");
+const tunaPizza = new Pizza(["tuna", "cheese", "sweet corn"], "Helen Azoia");
+console.log(salamiPizza);
+console.log(tunaPizza);
