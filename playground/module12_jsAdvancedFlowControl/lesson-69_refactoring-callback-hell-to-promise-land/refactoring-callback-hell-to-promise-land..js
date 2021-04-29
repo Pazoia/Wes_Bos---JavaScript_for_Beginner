5da2a8dd356991d80e68100b88351ff8bd9653ad
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-29 13:41:21 +0100

    Lesson 69 - Refactoring Callback Hell to Promise Land Complete

--- playground/module12_jsAdvancedFlowControl/lesson-69_refactoring-callback-hell-to-promise-land/refactoring-callback-hell-to-promise-land..js
+++ playground/module12_jsAdvancedFlowControl/lesson-69_refactoring-callback-hell-to-promise-land/refactoring-callback-hell-to-promise-land..js
@@ -1,5 +1,8 @@
 console.log("Callback Hell to Promise Land");
 
+/*
+    Below is Callback hell
+*/
 const go = document.querySelector(".go");
 
 go.addEventListener("click", function (e) {
@@ -33,3 +36,54 @@ go.addEventListener("click", function (e) {
         }, 500);
     }, 2000);
 });
+
+/*
+    Converting Callback hell into Promise Land
+*/
+
+const go1 = document.querySelector(".go1");
+
+function wait(ms = 0) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+function animate(e) {
+    const el = e.currentTarget;
+    // 1. Change the text to Go when clicked
+    el.textContent = "GO";
+
+    // 2. Make it a circle after 2s
+    wait(2000)
+        .then(() => {
+            el.classList.add("circle");
+
+            // 3. Make it red after 0.5s
+            return wait(500);
+        })
+        .then(() => {
+            el.classList.add("red");
+
+            // 4. Make it square after 0.25s
+            return wait(250);
+        })
+        .then(() => {
+            el.classList.remove("circle");
+
+            // 5. Make it purple after 0.3s
+            return wait(300);
+        })
+        .then(() => {
+            el.classList.remove("red");
+            el.classList.add("purple");
+
+            // 6. Fade out after 0.5s
+            return wait(500);
+        })
+        .then(() => {
+            el.classList.add("fadeOut");
+        });
+}
+
+go1.addEventListener("click", animate);
