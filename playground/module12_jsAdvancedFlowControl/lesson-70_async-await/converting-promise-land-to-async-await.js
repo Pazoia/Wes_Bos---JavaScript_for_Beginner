d2e15fbd7d94e0f0dbfde697d21eb14b4b0b362d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-29 18:51:13 +0100

    Lesson 70 - Converting promise land to async await

---
+++ playground/module12_jsAdvancedFlowControl/lesson-70_async-await/converting-promise-land-to-async-await.js
@@ -0,0 +1,89 @@
+console.log("Promise Land to Async await");
+
+/*
+    Below is Callback hell
+*/
+const go = document.querySelector(".go");
+
+go.addEventListener("click", function (e) {
+    const el = e.currentTarget;
+    // 1. Change the text to Go when clicked
+    el.textContent = "GO";
+
+    setTimeout(function () {
+        // 2. Make it a circle after 2s
+        el.classList.add("circle");
+
+        // 3. Make it red after 0.5s
+        setTimeout(function () {
+            el.classList.add("red");
+
+            // 4. Make it square after 0.25s
+            setTimeout(function () {
+                el.classList.remove("circle");
+
+                // 5. Make it purple after 0.3s
+                setTimeout(function () {
+                    el.classList.remove("red");
+                    el.classList.add("purple");
+
+                    // 6. Fade out after 0.5s
+                    setTimeout(function () {
+                        el.classList.add("fadeOut");
+                    }, 500);
+                }, 300);
+            }, 250);
+        }, 500);
+    }, 2000);
+});
+
+/*
+    Converting Callback hell into Promise Land
+*/
+
+const go1 = document.querySelector(".go1");
+
+function wait(ms = 0) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+function animate(e) {
+    const el = e.currentTarget;
+    // 1. Change the text to Go when clicked
+    el.textContent = "GO";
+
+    // 2. Make it a circle after 2s
+    wait(2000)
+        .then(() => {
+            el.classList.add("circle");
+
+            // 3. Make it red after 0.5s
+            return wait(500);
+        })
+        .then(() => {
+            el.classList.add("red");
+
+            // 4. Make it square after 0.25s
+            return wait(250);
+        })
+        .then(() => {
+            el.classList.remove("circle");
+
+            // 5. Make it purple after 0.3s
+            return wait(300);
+        })
+        .then(() => {
+            el.classList.remove("red");
+            el.classList.add("purple");
+
+            // 6. Fade out after 0.5s
+            return wait(500);
+        })
+        .then(() => {
+            el.classList.add("fadeOut");
+        });
+}
+
+go1.addEventListener("click", animate);
