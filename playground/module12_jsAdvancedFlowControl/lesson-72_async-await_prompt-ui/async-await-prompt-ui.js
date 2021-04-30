553d6aab86efa5667f7461ccd1bbdd0cf3da6c45
Paulo Azoia <pazoia.gomes@gmail.com>
2021-04-30 16:40:23 +0100

    Created function ask and pseudocode with requirements

--- playground/module12_jsAdvancedFlowControl/lesson-72_async-await_prompt-ui/async-await-prompt-ui.js
+++ playground/module12_jsAdvancedFlowControl/lesson-72_async-await_prompt-ui/async-await-prompt-ui.js
@@ -1,1 +1,10 @@
 console.log("Async Await Prompt UI");
+
+function ask(options) {
+    return new Promise(function (resolve) {
+        // Create a popup with all the fields in it
+        // Check if they want a cancel button
+        // listen for the submit event on the inputs
+        // Resolve data that was in the input box when submit button gets clicked
+    });
+}
