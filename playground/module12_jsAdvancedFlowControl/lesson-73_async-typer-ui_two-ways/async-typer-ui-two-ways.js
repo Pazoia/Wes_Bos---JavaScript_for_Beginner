b91ecb10ca0d4ccb9dd32fdcc835050322da0cc0
Paulo Azoia <pazoia.gomes@gmail.com>
2021-05-05 20:52:42 +0100

    Lesson 73 - Async Typer UI Complete

--- playground/module12_jsAdvancedFlowControl/lesson-73_async-typer-ui_two-ways/async-typer-ui-two-ways.js
+++ playground/module12_jsAdvancedFlowControl/lesson-73_async-typer-ui_two-ways/async-typer-ui-two-ways.js
@@ -9,17 +9,36 @@ function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
 }
 
 // async for of loop
-async function draw(el) {
+// async function draw(el) {
+//     const text = el.textContent;
+//     let soFar = "";
+//     for (const letter of text) {
+//         soFar += letter;
+//         el.textContent = soFar;
+//         // wait for some time
+//         const { typeMin, typeMax } = el.dataset;
+//         const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//         await wait(amountOfTimeToWait);
+//     }
+// }
+
+// recursion
+function draw(el) {
+    let index = 1;
     const text = el.textContent;
-    let soFar = "";
-    for (const letter of text) {
-        soFar += letter;
-        el.textContent = soFar;
-        // wait for some time
-        const { typeMin, typeMax } = el.dataset;
+    const { typeMin, typeMax } = el.dataset;
+    async function drawLetter() {
+        el.textContent = text.slice(0, index);
+        index += 1;
         const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
         await wait(amountOfTimeToWait);
+        // exit condition
+        if (index <= text.length) {
+            drawLetter();
+        }
     }
+    // when this function draw() runs, kick off drawLetter
+    drawLetter();
 }
 
 document.querySelectorAll("[data-type]").forEach(draw);
