9d7d02e90756b8fee61c3fbdb28267e12bfb1ca6
Paulo Azoia <pazoia.gomes@gmail.com>
2021-05-06 15:29:41 +0100

    Lesson 76 - Dad Joke Fetcher Complete

--- playground/module13_jsAjaxAndFetchingData/lesson-76_dad-jokes/dad-jokes.js
+++ playground/module13_jsAjaxAndFetchingData/lesson-76_dad-jokes/dad-jokes.js
@@ -2,6 +2,7 @@ console.log("Dad Jokes");
 
 const jokeButton = document.querySelector(".getJoke");
 const jokeHolder = document.querySelector(".joke p");
+const loader = document.querySelector(".loader");
 
 const buttonText = [
     "Ugh.",
@@ -15,12 +16,15 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
+    // turn loader on
+    loader.classList.remove("hidden");
     const response = await fetch("https://icanhazdadjoke.com", {
         headers: {
             Accept: "application/json",
         },
     });
     const data = await response.json();
+    loader.classList.add("hidden");
     return data;
 }
 
