23352714b581c8edd3eecf747a2ed34ed7e74eee
Paulo Azoia <pazoia.gomes@gmail.com>
2021-05-21 17:59:05 +0100

    timeData being displyed and responding to sounds

--- playground/module15_jsFinalRoundOfExercises/lesson-85_audio-visualization/sounds.js
+++ playground/module15_jsFinalRoundOfExercises/lesson-85_audio-visualization/sounds.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 const WIDTH = 1500;
 const HEIGHT = 1500;
 const canvas = document.querySelector("canvas");
@@ -5,6 +6,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 let analyser;
+let bufferLength;
 
 function handleError(err) {
     console.log("You must give permission to proceed!");
@@ -19,9 +21,43 @@ async function getAudio() {
     // How much data should be collected
     analyser.fftSize = 2 ** 10;
     // Pull the data off the audio
-    const timeData = new Uint8Array(analyser.frequencyBinCount);
-    const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    console.log(frequencyData);
+    // How many pieces of data are there
+    bufferLength = analyser.frequencyBinCount;
+    const timeData = new Uint8Array(bufferLength);
+    const frequencyData = new Uint8Array(bufferLength);
+    drawTimeData(timeData);
+}
+
+function drawTimeData(timeData) {
+    // inject the time data into our timeData array
+    analyser.getByteTimeDomainData(timeData);
+
+    // turning the data collected into something visual
+    // 1. Clear the canvas TODO
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    // 2. Setup some canvas drawing
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "orange";
+    ctx.beginPath();
+    const sliceWidth = WIDTH / bufferLength;
+    let x = 0;
+    timeData.forEach((data, i) => {
+        const v = data / 128;
+        const y = (v * HEIGHT) / 2;
+        // draw the lines
+        if (i === 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+        x += sliceWidth;
+    });
+
+    ctx.stroke();
+
+    // call itself as soon as possible
+    requestAnimationFrame(() => drawTimeData(timeData));
 }
 
 getAudio();
